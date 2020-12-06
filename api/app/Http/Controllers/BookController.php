ce5719a9c3a37669593185ce97feac152973b567
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2020-12-06 12:52:14 +0200

    implemented CRUD operations on BookController.php using the BookService.php added exception handlers on the API gateway

--- api/app/Http/Controllers/BookController.php
+++ api/app/Http/Controllers/BookController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\AuthorService;
 use App\Services\BookService;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BookController extends Controller
 {
@@ -16,11 +18,11 @@ class BookController extends Controller
     /**
      * @var BookService
      */
-    protected $service;
+    protected $bookService;
 
-    public function __construct(BookService $service)
+    public function __construct(BookService $bookService)
     {
-        $this->service = $service;
+        $this->bookService = $bookService;
     }
 
     /**
@@ -29,49 +31,67 @@ class BookController extends Controller
      */
     public function index()
     {
-        $response = $this->service->performRequest('get','lumen.books.test/books');
-        return $this->successResponse($response);
+        return $this->successResponse($this->bookService->all());
     }
 
     /**
      * Show a single book
      * @param string $book
-     * @return Response
+     * @return JsonResponse
      */
     public function show($book)
     {
-
+        return $this->successResponse($this->bookService->find($book));
     }
 
     /**
      * Create a new Book
      * @param Request $request
-     * @return Response
+     * @param AuthorService $authorService
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(Request $request, AuthorService $authorService)
     {
 
+        if( $request->has('author_id'))
+        {
+            $authorService->find($request->author_id);
+        }
+
+        return $this->successResponse(
+            $this->bookService->create($request->all()),
+            Response::HTTP_CREATED
+        );
     }
 
     /**
      * Update the data of a given Book
      * @param Request $request
      * @param string $book
-     * @return Response
+     * @param AuthorService $authorService
+     * @return JsonResponse
      */
-    public function update(Request $request, $book)
+    public function update(Request $request, $book,  AuthorService $authorService)
     {
+        if( $request->has('author_id'))
+        {
+            $authorService->find($request->author_id);
+        }
 
+        return $this->successResponse($this->bookService->update($book,$request->all()));
     }
 
     /**
      * Delete a given Book
      * @param string $book
-     * @return Response
+     * @return JsonResponse
      */
     public function destroy($book)
     {
-
+        return $this->successResponse(
+            $this->bookService->destroy($book),
+            Response::HTTP_NO_CONTENT
+        );
     }
 
 
--- api/app/Services/BookService.php
+++ api/app/Services/BookService.php
@@ -15,5 +15,48 @@ class BookService
         $this->baseUri = config('services.books.base_uri');
     }
 
+    /**
+     * Obtain a list of Authors from the books service
+     * @return string
+     */
+    public function all(){
+        return $this->performRequest('GET','books');
+    }
+
+    /**
+     * Obtain a single Author by their id
+     * @return string
+     */
+    public function find($bookId){
+        return $this->performRequest('GET',"/books/$bookId");
+    }
+
+    /**
+     * Create an author using the author service
+     * @return string
+     */
+    public function create($formData)
+    {
+        return $this->performRequest('POST','books',$formData);
+    }
+
+    /**
+     * Update an author using the author service
+     * @return string
+     */
+    public function update($bookId,$formData)
+    {
+        return $this->performRequest('PATCH',"/books/$bookId",$formData);
+    }
+
+
+    /**
+     * Remove an existing author using the author service
+     * @return string
+     */
+    public function destroy($bookId)
+    {
+        return $this->performRequest('DELETE',"/books/$bookId");
+    }
 
 }
